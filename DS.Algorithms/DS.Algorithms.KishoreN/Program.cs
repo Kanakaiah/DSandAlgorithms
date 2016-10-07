d4e08d3d10c9880b860406a7a4649665eb54f997
Kanakaiah <kanakaiah.e@hotmail.com>
2016-10-07 01:31:41 -0700

    Project creation for Kishore bro

---
+++ DS.Algorithms/DS.Algorithms.KishoreN/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS.Algorithms.KishoreN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
