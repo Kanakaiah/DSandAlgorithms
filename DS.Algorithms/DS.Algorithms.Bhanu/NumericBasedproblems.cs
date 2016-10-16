13d7f2d51a4223230b6baa3e5539cb38bdb0d545
v-bhanup <v-bhanup@BHANUP>
2016-10-16 11:03:20 -0700

    Solutions for couple of problems: 1. Palindrome checking. 2. Reverse a number.

---
+++ DS.Algorithms/DS.Algorithms.Bhanu/NumericBasedproblems.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS.Algorithms.Bhanu
+{
+    public class NumericBasedProblems
+    {
+        public static void Test_ReverseGivenNumber()
+        {
+            int[] numbers = new int[] { -1234, 0, 1111, 5002 };
+
+            foreach(int num in numbers)
+            {
+                Console.WriteLine("Input Number: {0}, OutputNumber: {1}", num, ReverseNumber(num));
+            }
+        }
+
+        private static int ReverseNumber(int num)
+        {
+            int result = 0;
+            while (num != 0)
+            {
+                result = (result * 10) + num % 10;
+                num = num / 10;
+            }
+
+            return num < 0 ? (result * -1) : result;
+        }
+    }
+}
