d411a5fb23c936def58726aded42ae582b95588c
Kranthi <kchepuri@live.com>
2016-10-17 00:11:03 -0700

    Organized code and added Array Arithmetic

---
+++ DS.Algorithms/DS.Algorithms.Kranthi/StringArray/NumberTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DS.Algorithms.Kranthi.StringArray
+{
+    class NumberTest
+    {
+        internal static void Run()
+        {
+
+            long input = 1234;
+            Console.WriteLine("{0}-----> {1}", input, Number.Reverse(input));
+            input = -1234;
+            Console.WriteLine("{0}-----> {1}", input, Number.Reverse(input));
+            input = 123400;
+            Console.WriteLine("{0}-----> {1}", input, Number.Reverse(input));
+            input = 1307130571;
+            Console.WriteLine("{0}-----> {1}", input, Number.Reverse(input));
+            input = -1307130571;
+            Console.WriteLine("{0}-----> {1}", input, Number.Reverse(input));
+            input = 0;
+            Console.WriteLine("{0}-----> {1}", input, Number.Reverse(input));
+            input = -1;
+            Console.WriteLine("{0}-----> {1}", input, Number.Reverse(input));
+            input = -100;
+            Console.WriteLine("{0}-----> {1}", input, Number.Reverse(input));
+            input = 100001;
+            Console.WriteLine("{0}-----> {1}", input, Number.Reverse(input));
+
+            Console.Read();
+
+        }
+    }
+}
