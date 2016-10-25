9e44a39a7b3b57e9fe4298110daf0521f724a207
Srikanth R Thumma <mastersrikanth@gmail.com>
2016-10-24 21:15:06 -0500

    Nov 24 assignments

---
+++ DS.Algorithms/DS.Algorithms.Srikanth/ISCF_Tech_Nov24/Srik_MyDetails/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Srik_MyDetails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My Name is: {0}", "Srikanth");
+            Console.WriteLine("My Date of Birth: {0}", "29 Sep");
+            Console.WriteLine("My Address: ");
+            Console.WriteLine("{0}", "17751 NE 90th St.");
+            Console.WriteLine("{0} # {1}{2}", "Apt", 'D', 121);
+            Console.WriteLine("{0},{1} {2}", "Redmond", "WA", 98052);
+            Console.ReadLine();
+        }
+    }
+}
